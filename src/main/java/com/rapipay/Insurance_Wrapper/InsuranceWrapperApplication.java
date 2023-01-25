b061fb501c66bb4feba7f4474af74a25a36ecb48
vanshika <vanshika.arya@rapipay.com>
2023-01-25 11:24:45 +0530

    First Commit

---
+++ src/main/java/com/rapipay/Insurance_Wrapper/InsuranceWrapperApplication.java
@@ -0,0 +1,13 @@
+package com.rapipay.Insurance_Wrapper;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InsuranceWrapperApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InsuranceWrapperApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/rapipay/Insurance_Wrapper/InsuranceWrapperApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rapipay.Insurance_Wrapper;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InsuranceWrapperApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
