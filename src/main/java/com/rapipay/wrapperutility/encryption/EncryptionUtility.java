2a496795e159a77f412a75c886b6bd7a1b42c0ca
Faraz <mohd.khan@rapipay.com>
2023-01-25 11:56:56 +0530

    Test2 change

--- src/main/java/com/rapipay/wrapperutility/encryption/EncryptionUtility.java
+++ src/main/java/com/rapipay/wrapperutility/encryption/EncryptionUtility.java
@@ -3,6 +3,7 @@ package com.rapipay.wrapperutility.encryption;
 public class EncryptionUtility {
 
     public String helloWorld(){
+        //hello
         String str="sacasc";
         return "Hello World";
 
